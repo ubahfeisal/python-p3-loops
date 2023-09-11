524b4889ddc7d6f6c20f4d67b9bd91d329f4d448
ubah feisal <feisalubah1@gmail.com>
2023-09-11 15:25:19 +0300

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    # code goes here!
-    pass
+# def happy_new_year():
+#     # code goes here!
+#     pass
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+# def square_integers(int_list):
+#     # code goes here!
+#     pass
+
+# def fizzbuzz():
+#     # code goes here!
+#     pass
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
+fizzbuzz()
